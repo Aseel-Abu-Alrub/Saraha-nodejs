785ddea763e0db3648268f64592f730472bb7dfc
Aseel-Abu-Alrub <aseelabualrub98@gmail.com>
2023-09-29 23:57:18 +0300

    update2

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB=async()=>{
-    return await mongoose.connect("mongodb+srv://aseel:aseel123@cluster0.jpifjev.mongodb.net/sarahaa")
+    return await mongoose.connect(process.env.LOCAL_DB)
     .then(()=>{
         console.log("connct to DB");
     }).catch((err)=>{
